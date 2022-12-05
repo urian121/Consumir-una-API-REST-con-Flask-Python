752da1b18846e065ffbe270cfa60da9f6f0408a7
urian <urian1213viera@gmail.com>
2022-12-04 21:21:57 -0500

    ajustes terminados

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, jsonify
 import requests #Importando libreria Request para hacer peticiones HTTP
-import json 
+import json #Importando json desde python
+ 
  
 #Declarando nombre de la aplicación e inicializando, crear la aplicación Flask
 app = Flask(__name__) 
@@ -8,33 +9,46 @@ app.secret_key = '97110c78ae5105da20fe'
 
 
 #Creando mi Decorador para el Home
+@app.route('/API', methods=['GET','POST'])
+def api():
+    URL_API = 'https://jsonplaceholder.typicode.com/users/'
+    solic_req = requests.get(URL_API)
+    data_API = solic_req.json(); #Este método es conveniente cuando la API devuelve JSON
+    #Toda la información sobre nuestra petición está ahora almacenada en un objeto Response llamado solic_req
+    #print(solic_req.json())
+    #print(solic_req.status_code) #comprobar el estado de la peticion
+
+
+    if solic_req.status_code == 200:
+        print('Respuesta OK')
+        return jsonify({'resp': data_API })
+    else:
+        print('Respuesta Fallida')
+
+
+
+
+#Creando mi Decorador para el Home
 @app.route('/', methods=['GET','POST'])
 def inicio():
+    #URL_API = 'https://jsonplaceholder.typicode.com/todos/8'
     URL_API = 'https://fakestoreapi.com/products?limit=8'
     solic_req = requests.get(URL_API)
-    #print(solic_req.json()) #convirtiendo solicitu en formato json
-    data_API = solic_req.json();
+    print(solic_req)
+    #print(solic_req.json()) #convirtiendo solicitud en formato json
+    data_API = solic_req.json(); #Este método es conveniente cuando la API devuelve JSON
     #Toda la información sobre nuestra petición está ahora almacenada en un objeto Response llamado solic_req
-    #print(res.status_code) #comprobar el estado de la peticion
-    #print(res.headers)
-    #print(res.text)
-    #json = res.json()
-    #print(json)
-    '''parametros = dict(key=API_KEY, text='Hello', lang='en-es')
-        res = requests.get(URL_API, parametros=parametros)
-        
-        query = {'q': 'Forest', 'order': 'popular', 'min_width': '800', 'min_height': '600'}
-        req = requests.get('https://pixabay.com/en/photos/', params=query)
-        req.url
-    '''
+    print(solic_req.status_code) #comprobar el estado de la peticion
 
 
-    if solic_req:
+    if solic_req.status_code == 200:
         print('Respuesta OK')
     else:
         print('Respuesta Fallida')
 
     return render_template('public/index.html', miData= data_API)
+
+    
 
 
 #Redireccionando cuando la página no existe
